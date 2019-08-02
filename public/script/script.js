733d2b55910633b3e2f63a5b14b00167c1ed0df3
Suin Choi <susu5824@nate.com>
2019-08-02 10:05:44 +0900

    plaese

--- public/script/script.js
+++ public/script/script.js
@@ -1,5 +1,19 @@
-function log(){
-	console.log("please");
-	var a = document.getElementById("navi_menu");
-	a.style.disply = block;
-}
+$(document).ready(function(){
+    $("#menu").click(function(){
+      $(".navi_menu").slideDown();
+      $(".navigator").css("background-color", "#111010");
+      $(".navigator").css("height", "200px");
+      $("#x").slideDown();
+      $("#menu").hide();
+      $("#name").hide();
+    });
+      $("#x").click(function(){
+        $(".navi_menu").hide();
+        $("#menu").show();
+        $("#name").show();
+        $("#x").hide();
+        $(".navigator").css("background-color", "white");
+        $(".navigator").css("height", "50px");
+        $("#menu").css("color", "#111010");
+      });
+  });
