f879b261f1c313fba8006c49bbde12ba6b2fd410
Suin Choi <susu5824@nate.com>
2019-08-02 09:35:06 +0900

    plaese

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const port = process.env.PORT || 8686;
 
 //app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(__dirname + '/public'));
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
