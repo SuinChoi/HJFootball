96f358e7d25cdd281f997107316e78d14216d4c8
Suin Choi <susu5824@nate.com>
2019-08-02 09:58:37 +0900

    plaese

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const app = express();
 const server = require('http').Server(app); 
 const port = process.env.PORT || 8686; 
 
+app.set('view engine', 'ejs');
 //app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+   res.render('index');
 });
 server.listen(port, function(){
   // when this server is opened 
