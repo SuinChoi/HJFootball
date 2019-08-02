94cd67e1f7ab3420783b9853d3f04681c6fdd4b1
Suin Choi <susu5824@nate.com>
2019-08-02 15:46:20 +0900

    lego

--- public/script/script.js
+++ public/script/script.js
@@ -1,30 +1,36 @@
 $(document).ready(function(){
-	function startWorker(){
-		if(typeof(Worker)!=="undefined"){
-			if(typeof(w)=="undefined"){
-				w = new Worker("webworker.js");
-			}
-			w.onmessage = function(event){
-				$(".home_content").css("background-image","url('img/jg2')");
-			}
-		}
-
+	var slideIndex = 0;
+	showSlides();	
+	var i;
+	var slides = document.getElementsByClassName("mySlides");
+	var dots = document.getElementsByClassName("dot");
+	for (i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";  
 	}
+	slideIndex++;
+	if (slideIndex > slides.length) {slideIndex = 1}    
+		for (i = 0; i < dots.length; i++) {
+			dots[i].className = dots[i].className.replace(" active", "");
+		}
+		slides[slideIndex-1].style.display = "block";  
+		dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
+
     $("#menu").click(function(){
-      $(".navi_menu").slideDown();
-      $(".navigator").css("background-color", "#111010");
-      $(".navigator").css("height", "200px");
-      $("#x").slideDown();
-      $("#menu").hide();
-      $("#name").hide();
+    	$(".navi_menu").slideDown();
+    	$(".navigator").css("background-color", "#111010");
+    	$(".navigator").css("height", "200px");
+    	$("#x").slideDown();
+    	$("#menu").hide();
+    	$("#name").hide();
     });
-      $("#x").click(function(){
-        $(".navi_menu").hide();
-        $("#menu").show();
-        $("#name").show();
-        $("#x").hide();
-        $(".navigator").css("background-color", "white");
-        $(".navigator").css("height", "50px");
-        $("#menu").css("color", "#111010");
-      });
-  });
+    $("#x").click(function(){
+    	$(".navi_menu").hide();
+    	$("#menu").show();
+    	$("#name").show();
+    	$("#x").hide();
+    	$(".navigator").css("background-color", "white");
+    	$(".navigator").css("height", "50px");
+    	$("#menu").css("color", "#111010");
+    });
+});
