e91045b4519005b25abb6ed37be026925186635a
Suin Choi <susu5824@nate.com>
2019-08-02 09:08:06 +0900

    gooo

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express'); 
 const app = express(); 
-const server = require('http').Server(app);
-const path = require('path'); 
+const server = require('http').Server(app); 
 const port = process.env.PORT || 8686; 
 
 //app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname + '/public'));
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
