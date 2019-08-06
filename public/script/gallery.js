6abc4a989b23ef2832a9c6b64f0542cd247e5a41
Suin Choi <susu5824@nate.com>
2019-08-06 16:21:04 +0900

    le

--- public/script/gallery.js
+++ public/script/gallery.js
@@ -24,7 +24,7 @@ function openModal(img) {
 	$('.mySlides').append(add);
 	console.log("a " + add);
 	$('#myModal').css("display","block");
-	console.log("b " +$('.mySlides').src);
+	console.log("b " +$('.mySlides').attr('src'));
 }
 
 	// Close the Modal
