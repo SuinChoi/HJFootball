80d0a66aadb120e54cf6a61dd7dbd4d947e1f82b
Suin Choi <susu5824@nate.com>
2019-08-06 17:05:22 +0900

    lego

--- public/script/gallery.js
+++ public/script/gallery.js
@@ -2,7 +2,7 @@
 // Open the Modal
 function openModal(img) {
 	console.log(img.getAttribute('src'));
-	document.getElementById("Pic").setAttribute('src',img.getAttribute('src') )  ;
+	document.getElementById("Pic").setAttribute('src',img.getAttribute('src'))  ;
 	console.log(document.getElementById("Pic").getAttribute('src'));
 	document.getElementById("myModal").style.display = "block";
 }
