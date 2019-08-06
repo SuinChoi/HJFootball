f9c1cec23aa252c0a1fbc73d09cb44832ddbc4b9
Suin Choi <susu5824@nate.com>
2019-08-06 16:59:44 +0900

    lego

--- public/script/gallery.js
+++ public/script/gallery.js
@@ -1,7 +1,7 @@
 
 // Open the Modal
 function openModal(img) {
-	console.log(img.src);
+	console.log(img.getAttribute('src');
 	document.getElementById("Pic").src = img.src;
 	console.log(document.getElementById("Pic").src);
 	document.getElementById("myModal").style.display = "block";
