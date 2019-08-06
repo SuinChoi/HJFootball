8fb9381412143779341152e4de79d69654961dcc
Suin Choi <susu5824@nate.com>
2019-08-06 16:32:22 +0900

    le

--- public/script/gallery.js
+++ public/script/gallery.js
@@ -1,35 +1,12 @@
-$(document).ready(function(){
-	putPics();
-	function putPics(){	
-		var array = ["jg1", "hg2","hgg2",
-		"hg8","jjg1","hg3",
-		"jjg1", "hgg1","jg1",
-		"hg1" , "hg9","jjg3"];
-		for(var i=0; i<array.length; i++){
 
-			var text = '<div class="column">';
-			text += '<img src=';
-			text += '"img/' + array[i] + '.jpg" ';
-			text += 'onclick="openModal(this);" class="hover-shadow"></div>';
-			$('.row').append(text);
-		}
-		
-	}	
-});
 // Open the Modal
 function openModal(img) {
-	var temp = img.getAttribute('src');
-	console.log("temp " + temp);
-	var add ='<img id="Pic" src="' + temp + '">';
-	$('.mySlides').append(add);
-	console.log("a " + add);
-	$('#myModal').css("display","block");
-	console.log("b " +$('#Pic').attr('src'));
+	document.getElementById("Pic").src = img.getAttribute('src');
+	document.getElementById("myModal").style.display = "block";
 }
 
 	// Close the Modal
 	function closeModal() {
-		//$('#Pic').
 		document.getElementById("myModal").style.display = "none";
 	}
 
