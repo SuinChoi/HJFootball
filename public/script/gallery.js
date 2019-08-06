da769616d74ef06c140d030a590f7c101baa1027
Suin Choi <susu5824@nate.com>
2019-08-06 14:36:57 +0900

    le

--- public/script/gallery.js
+++ public/script/gallery.js
@@ -9,3 +9,9 @@ function openModal(img) {
 function closeModal() {
   document.getElementById("myModal").style.display = "none";
 }
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
